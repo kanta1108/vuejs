1e93749e8de4ed2504923033f3587460e07dd79a
k-suzuki <k-suzuki@signalinc.co.jp>
2023-01-27 15:11:49 +0900

    1/27

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">
+        <p>Good</p>
+        <p>Good</p>
+    </div><!-- /.header -->
+</template>
+<script>
+// export default {
+    
+// }
+</script>
+<style scoped>
+div{
+    border: 2px dashed #000;
+}
+p{
+    color: blue;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import LikeNumber from "./components/LikeNumber";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true;
+
+Vue.component("LikeNumber", LikeNumber);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
